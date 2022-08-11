397e66bae1e9a201da6217e59968d42d2844b72a
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-11 23:11:40 +0200

    styling the nav bar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 // import { BiArchive } from "react-icons/bi";
 
 import Auth from '../../utils/auth';
+
+const styles = {
+  Nav: {
+    background: "#f3233a",
+  },
+  NavLink: {
+    color: "#F5F8F7",
+  },
+};
 
 const Header = () => {
   const logout = (event) => {
@@ -11,21 +22,21 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <Nav>
+    <Nav style={styles.Nav}  className="justify-content-end me-auto" expand="lg">
       <Nav.Item>
-        <Nav.Link as={Link} to="/">
+        <Nav.Link style={styles.NavLink} as={Link} to="/">
           Home
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} to="/profile">
+        <Nav.Link style={styles.NavLink} as={Link} to="/profile">
           Profile
         </Nav.Link>
       </Nav.Item>
       {Auth.loggedIn() ? (
         <>
           <Nav.Item>
-            <Nav.Link onClick={logout}>
+            <Nav.Link style={styles.NavLink} onClick={logout}>
               Logout
             </Nav.Link>
           </Nav.Item>
@@ -33,12 +44,12 @@ const Header = () => {
       ) : (
         <>
           <Nav.Item>
-            <Nav.Link as={Link} to="/login">
+            <Nav.Link style={styles.NavLink} as={Link} to="/login">
               Login
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link as={Link} to="/signup" >
+            <Nav.Link style={styles.NavLink} as={Link} to="/signup" >
               Signup
             </Nav.Link>
           </Nav.Item>
