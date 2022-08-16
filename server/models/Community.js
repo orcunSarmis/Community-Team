e4e39816e9edeee6779a3c9312b0037ec9c591e5
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-16 16:43:29 +1000

    community model completed

--- server/models/Community.js
+++ server/models/Community.js
@@ -2,10 +2,25 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const communitySchema = new Schema({
-    communityName: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        users: [
+            {
+                type: Schema.Types.ObjectId,
+                ref : 'User'
+            }
+        ]
     }
-})
+);
+
+const Community = model('Community', communitySchema);
+
+module.exports = Community;
