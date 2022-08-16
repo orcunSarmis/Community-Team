a33ac72d5eb694d15970d4b5bee36a425065e450
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-16 22:26:36 +1000

    code clean up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -8,9 +8,9 @@ import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
+    username: "",
+    email: "",
+    password: "",
   });
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
