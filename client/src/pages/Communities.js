cfcbc3052c535f78a4b5ab6daf734c9753a84fc2
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-17 21:31:29 +1000

    all working good

--- client/src/pages/Communities.js
+++ client/src/pages/Communities.js
@@ -31,61 +31,6 @@ const Communities = () => {
     </div>
   );
 
-  // return (
-  //     <>
-  //     <p
-  //       style={{ background: "linear-gradient( 11deg,yellow,green, #811717)" }}
-  //       className="text-center bg-dark text-white fs-4"
-  //     >
-  //       Profile Page
-  //     </p>
-  //     <div className="container ">
-  //       <div className="row">
-  //         <div className="twPc-div">
-  //           <div className="twPc-button"></div>
-  //           <a className="twPc-bg twPc-block"></a>
-  //           <div>
-  //             <a title="" href="" className="twPc-avatarLink">
-  //               <img alt="" src={ProPic} className="twPc-avatarImg" />
-  //             </a>
-  //             <div className="twPc-divUser">
-  //               <div className="twPc-divName">
-  //                 <a href="">{profileData.username}</a>
-  //               </div>
-  //               <span>
-  //                 <a href="">
-  //                   @<span>{profileData._id}</span> - [{profileData.email}]
-  //                 </a>
-  //               </span>
-  //             </div>
-  //             <div className="twPc-divStats">
-  //               <ul className="twPc-Arrange d-flex justify-content-around">
-  //                 <li className="twPc-ArrangeSizeFit">
-  //                   <a href="" title="3 events">
-  //                     <span className="twPc-StatLabel twPc-block">Events</span>
-  //                     <span className="twPc-StatValue">3</span>
-  //                   </a>
-  //                 </li>
-  //                 <li className="twPc-ArrangeSizeFit">
-  //                   <a href="" title="885 Following">
-  //                     <span className="twPc-StatLabel twPc-block">Following</span>
-  //                     <span className="twPc-StatValue">885</span>
-  //                   </a>
-  //                 </li>
-  //                 <li className="twPc-ArrangeSizeFit">
-  //                   <a href="" title="1.810 Followers">
-  //                     <span className="twPc-StatLabel twPc-block">Followers</span>
-  //                     <span className="twPc-StatValue">1.810</span>
-  //                   </a>
-  //                 </li>
-  //               </ul>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </>
-  // );
 };
 
 export default Communities;
