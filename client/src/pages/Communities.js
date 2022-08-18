59c143ddd99cba4760f6e2858eaecf59b0d01e5a
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-18 20:33:30 +1000

    img added

--- client/src/pages/Communities.js
+++ client/src/pages/Communities.js
@@ -1,6 +1,8 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { QUERY_COMMUNITIES } from "../utils/queries";
+import Img3 from "../img/img3.jpg";
+import Img4 from "../img/brisbane.jpg";
 
 const Communities = () => {
   const { loading, data } = useQuery(QUERY_COMMUNITIES);
@@ -13,18 +15,31 @@ const Communities = () => {
   const communities = data.communities || [];
 
   return (
-    <div>
-      <h2>Communities</h2>
-      {communities.map((el) => {
-        return (
-          <div>
-            <p>{el.name}</p>
-            <p>{el.location}</p>
-            <button>Add</button>
-          </div>
-        );
-      })}
+    <>
+    <div className="d-flex justify-content-between">
+    <h2 className="p-4 ">Communities </h2>
+     <a href='AddCommunities' className="me-4 mt-3 fs-3">Add Communities</a>
     </div>
+   <div className="d-flex ms-4 gap-2">
+     {communities.map((el,index) => {
+       return (
+     <div key={index} className="card m-0 mt-2 " style={{width: "18rem"}}>
+       <div className="card-body">
+         <h5 className="card-title">{el.name}</h5>
+         <p className="card-text">
+         {el.location}
+         </p>
+         <a href="#" className="btn btn-primary">
+         Add
+         </a>
+        <img src={Img4} style={{borderRadius:"20px"}} className="w-50"/>
+       </div>
+     </div>
+     );
+     })}
+    
+   </div>
+   </>
   );
 
 };
